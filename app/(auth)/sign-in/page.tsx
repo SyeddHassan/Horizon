381ba033f87e0a03100ffdda192ab945847e483f
SyeddHassan <syeddhassanali@gmail.com>
2024-09-15 16:25:52 -0700

    implementation of sign-in and sign-up page, with two new components: auth-form and custom input

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import AuthForm from "@/components/auth-form";
+
 const SignIn = () => {
-  return <section className="flex-center size-full max-sm:px-6"></section>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
+  );
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,11 @@
-const SignUp = () => {
-  return <section className="flex-center size-full max-sm:px-6"></section>;
+import AuthForm from "@/components/auth-form";
+
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 };
 
 export default SignUp;
