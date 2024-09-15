d98fb84ba6be86cc79efb5610e88ce3b1e96bc86
SyeddHassan <syeddhassanali@gmail.com>
2024-09-15 14:29:03 -0700

    implement header box component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
+import HeaderBox from "@/components/header-box";
+
 const Home = async () => {
+  const loggedIn = { firstName: "Hassan" };
+
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          {/*Header Box Component Here */}
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
           {/* Total Balance Box Component Here */}
         </header>
