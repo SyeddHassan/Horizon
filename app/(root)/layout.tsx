e8a515dc5e5adc3f4b4d8ed20d9dc30510c6766f
SyeddHassan <syeddhassanali@gmail.com>
2024-09-15 15:16:02 -0700

    implememnt mobile navbar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import Sidebar from "@/components/side-bar";
+import MobileNav from "@/components/mobile-navbar";
 
 export default async function RootLayout({
   children,
@@ -16,7 +17,9 @@ export default async function RootLayout({
       <div className="flex size-full flex-col">
         <div className="root-layout">
           <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
-          <div>{/* MobileNav Component Here */}</div>
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
         </div>
         {children}
       </div>
