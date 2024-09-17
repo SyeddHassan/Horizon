e716a38a77f4d4df3c11de3015136350c8eab43e
SyeddHassan <syeddhassanali@gmail.com>
2024-09-16 18:00:54 -0700

    my banks page updated

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 
+import BankCard from "@/components/bank-card";
+import HeaderBox from "@/components/header-box";
+
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
 const MyBanks = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
   return (
     <section className="flex">
       <div className="my-banks">
-        {/* Header Box Component Here */}
+        <HeaderBox
+          title="My Bank Accounts"
+          subtext="Effortlessly manage your banking activites."
+        />
+
         <div className="space-y-4">
           <h2 className="header-2">Your cards</h2>
           <div className="flex flex-wrap gap-6">
-            {/* Bank Cards Component Here*/}
+            {accounts &&
+              accounts.data.map((a: Account) => (
+                <BankCard
+                  key={accounts.id}
+                  account={a}
+                  userName={loggedIn?.firstName}
+                />
+              ))}
           </div>
         </div>
       </div>
