754305bf4712524dc21cea3fa3c316f754a6ee5f
SyeddHassan <syeddhassanali@gmail.com>
2024-09-16 18:44:51 -0700

    finial changes made

--- components/right-sidebar.tsx
+++ components/right-sidebar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { countTransactionCategories } from "@/lib/utils";
 
 import BankCard from "./bank-card";
+import Category from "./category";
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   const categories: CategoryCount[] = countTransactionCategories(transactions);
@@ -64,7 +65,11 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="mt-10 flex flex-1 flex-col gap-6">
           <h2 className="header-2">Top categories</h2>
 
-          <div className="space-y-5">{/* Categories Component Here */}</div>
+          <div className="space-y-5">
+            {categories.map((category, index) => (
+              <Category key={category.name} category={category} />
+            ))}
+          </div>
         </div>
       </section>
     </aside>
